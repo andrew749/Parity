ec8e7ac1f9847517d6498e7060b42d2f4a85228f
Andrew Codispoti <andrewcod749@gmail.com>
2015-07-24 21:54:07 -0400

    print list in reverse order.

--- parity.py
+++ parity.py
@@ -5,6 +5,13 @@ from urllib.request import urlopen
 import webbrowser
 userinput = "placeholder"
 muzik_url = "http://muzik-api.herokuapp.com/search?%s"
+
+def printSongOptions(songArray):
+    for result in reversed(songArray):
+        print ('{}.  Name: {}'.format(result[2],result[0]))
+        print ('      URL: {}'.format(result[1]))
+        print('\n')
+
 while( not userinput == ""):
         songname = input("Enter the name of the song to download: ")
         data = urlencode({'songname':songname})
@@ -16,11 +23,9 @@ while( not userinput == ""):
         songArray = []
         for result in jsonresults["url"]:
             for key,value in  result.items():
-                print ('{}.  Name: {}'.format(count,key))
-                print ('      URL: {}'.format(value))
-                print('\n')
-                songArray.append((key,value))
+                songArray.append((key,value,count))
             count+=1
+        printSongOptions(songArray)
         userinput_2 = int(input('Select a song to try'))
         while(not userinput_2 == ''):
             webbrowser.open(songArray[userinput_2][1])
@@ -30,4 +35,6 @@ while( not userinput == ""):
                 f.write(urlopen(songArray[userinput_2][1]))
                 break
             userinput_2 = int(input('Select a song to try'))
+            printSongOptions(songArray)
+
 
